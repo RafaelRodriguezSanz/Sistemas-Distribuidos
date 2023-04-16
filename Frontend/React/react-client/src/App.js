644d0fc40a14c120275e6812b4068c71471f8e66
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2023-04-16 12:52:35 -0300

    Added basic front

---
+++ Frontend/React/react-client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LeafletMap from './Components/LeafletMap';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <LeafletMap />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/React/react-client/src/Components/LeafletMap.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { MapContainer  , TileLayer, Marker, Popup } from 'react-leaflet';
+
+const position = [-34.90328, -56.18816];
+
+class LeafletMap extends Component {
+    render() {
+      return (
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+          />
+        </MapContainer>
+      );
+    }
+  }
+
+export default LeafletMap;
