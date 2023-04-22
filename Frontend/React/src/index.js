1bdc0636ff06287059a49824be7b7756ccfe0660
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2023-04-21 21:38:40 -0300

    Added initial frontend

---
+++ Frontend/React/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Login from './Pages/Login';
+import LeafletMap from './Pages/LeafletMap';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/map" element={<LeafletMap />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+reportWebVitals(console.log);
