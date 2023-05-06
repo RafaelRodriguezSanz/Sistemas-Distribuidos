d832d18a9621077978927b8cd7039040f180a76c
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2023-05-06 13:58:08 -0300

    added integration

--- Frontend/React/src/Components/LeafletMapComponent.jsx
+++ Frontend/React/src/Components/LeafletMapComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState  } from "react";
+import React, { useEffect, useState  } from "react";
 import { MapContainer  , TileLayer, Marker, Popup, CircleMarker } from 'react-leaflet';
 import L from 'leaflet';
 import '../App.css';
@@ -19,57 +19,61 @@ const icon = L.icon({
 
 const latitud = -34.90328
 const longitud = -56.18816
-// const flow = 53
 const center = [latitud, longitud];
 const url = 'http://localhost:8086'
 const token = '0qfrVjTKP-s9VZ_msUoZqez5ubytxENzCZo93WNE1_rzI-2k59tTLcRTMHCsWlxm67paxbCKInl0b8HzG5PzLg=='
 const queryApi = new InfluxDB({url , token}).getQueryApi('UCU')
 
-const [data, setData] = useState([])
+const MILI_SECONDS = 5000;
 
-const MINUTE_MS = 5000;
+function LeafletMapComponent() {
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    queryRaw()
-  }, MINUTE_MS);
-  return () => clearInterval(interval);
-}, [])
-
-async function queryRaw() {
-  data = [];
-  const fluxQuery = `
-    from(bucket: "DATA")
-    |> range(start: -60s)
-    |> filter(fn: (r) => r["_measurement"] == "metric")
-    |> filter(fn: (r) => r["_field"] == "flow")
-    |> filter(fn: (r) => r["host"] == "host1")
-    |> group(columns: ["latitud", "longitud"])
-    |> group(columns: ["Montevideo"])
-  `
-  // const queryResult = queryApi.collectRows(fluxQuery);
-  // const result = await queryApi.queryRaw(fluxQuery)
-  // console.log('\nQueryRaw SUCCESS>')
-  // console.log(result)
+  let [state, setState] = useState([])
   
-  for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
-    const o = tableMeta.toObject(values)
-    console.log(
-      `latitud:  ${o._latitud} 
-      longitud: ${o._longitud}`
-      )
-    data.push({
-      latitud: o._latitud,
-      longitud: o._longitud,
-      flow: o._flow 
-    });
-  }
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      let data = []
+      const fluxQuery = `
+        from(bucket: "DATA")
+        |> range(start: -5s)
+        |> filter(fn: (r) => r["host"] == "host1")
+        |> filter(fn: (r) => r["_measurement"] == "metric")
+          `
+        
+        let latitudValue=0
+        let longitudValue=0
+        let flowValue=0
+        try {
+          for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
+            const o = tableMeta.toObject(values)
+            if (o._field === "latitude") {
+              latitudValue = o._value
+            }
+            if (o._field === "longitud") {
+              longitudValue = o._value
+            }
+            if (o._field === "flow") {
+              flowValue = o._value
+            }
+          }
+        } catch (error) {
+          console.log("InfluxDB Unavailable!")
+          latitudValue = -34.90328;
+          longitudValue =  -56.18816;
+          flowValue =  43;
+        }
+        data.push({
+          flow: flowValue,
+          center: [latitudValue, longitudValue],
+          index : data.length
+        });
+        setState(data);
+          
+    }, MILI_SECONDS);
+    return () => clearInterval(interval);
+  }, [])
 
-}
-
-
-class LeafletMapComponent extends Component {
-  render(){
+  
     return (
       <Paper elevation={3}>
           <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
@@ -77,25 +81,22 @@ class LeafletMapComponent extends Component {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
             />
-            <CircleMarker center={center} pathOptions={{ color: "rgba(117, 248, 255, 25)" }} radius={30}>
-              <Marker center={center} icon={icon}>
-              {data.map((values) => 
-                <Popup> 
-                  Latitud: {values.latitud}
-                  <br /> 
-                  Longitud: {values.longitud}
-                  <br /> 
-                  Caudal: {values.flow} m³/s
-                </Popup>
+              {state.map((values) => 
+                <CircleMarker  key={values.index + "-CircleMarker" } center={values.center} pathOptions={values.flow > 45 ? { color: "rgba(117, 248, 255, 25)" } : { color: "rgba(255, 118, 118, 25)" }} radius={30}>
+                  <Marker key={values.index + "-Marker"} position={values.center} icon={icon}>
+                    <Popup key={values.index + "-Popup"} > 
+                      Latitud: {values.center[0]}
+                      <br /> 
+                      Longitud: {values.center[1]}
+                      <br /> 
+                      Caudal: {values.flow} m³/s
+                    </Popup>
+                  </Marker>
+                </CircleMarker>
               )}
-              
-              </Marker>
-            </CircleMarker>
           </MapContainer>
-          <button onClick={queryRaw}>Click me</button>
       </Paper>
     );
-  }
 };
 
 
