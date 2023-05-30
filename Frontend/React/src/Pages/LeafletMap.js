1e51600655182e320d91f1bfe13c257da6f173d1
Rafael Rodriguez <69221486+RafaelRodriguezSanz@users.noreply.github.com>
2023-05-30 16:37:14 -0300

    Feature/authentication (#33)
    * Added authentication
    
    * Added User Icon

--- Frontend/React/src/Pages/LeafletMap.js
+++ Frontend/React/src/Pages/LeafletMap.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../App.css';
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 import LeafletMapComponent from '../Components/LeafletMapComponent';
 import DrawerComponent from '../Components/DrawerComponent';
 
 function LeafletMap() {
+  const { isAuthenticated, logout, user } = useAuth0();
   return (
     <div className="LeafletMap">
       <>
-        <DrawerComponent content={LeafletMapComponent}></DrawerComponent>
+        <div>
+        </div>
+        <DrawerComponent logout={logout} avatarimage={ user.picture } name={ user.name  } content={LeafletMapComponent}></DrawerComponent>
       </>
     </div>
   );
 }
-
-export default LeafletMap;
+export default withAuthenticationRequired(LeafletMap);
